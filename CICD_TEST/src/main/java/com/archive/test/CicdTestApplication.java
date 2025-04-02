1bb703d8a633264a1cd1967f0ee11695bcc5385a
taejun123123 <skxowns222@naver.com>
2025-04-02 23:43:03 +0900

    commit

---
+++ CICD_TEST/src/main/java/com/archive/test/CicdTestApplication.java
@@ -0,0 +1,13 @@
+package com.archive.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdTestApplication.class, args);
+	}
+
+}
